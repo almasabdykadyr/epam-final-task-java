eb613de4c7cf26f1e9c37c3389857f624c80dc84
almasabdykadyr <almasabdykadir@gmail.com>
2024-10-14 00:16:32 +0500

    some edits

--- customer-service/src/main/java/com/almasabdykadyr/customerservice/customer/CustomerResponse.java
+++
@@ -1,8 +0,0 @@
-package com.almasabdykadyr.customerservice.customer;
-
-public record CustomerResponse(
-        String firstname,
-        String lastname,
-        String email,
-        Address address) {
-}
--- customer-service/src/main/java/com/almasabdykadyr/customerservice/customer/ErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package com.almasabdykadyr.customerservice.customer;
-
-import java.util.Map;
-
-public record ErrorResponse(
-        Map<String, String> errors
-) {
-}
